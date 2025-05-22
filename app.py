8a7078f8bf276f55e0a0f76028ddb6126bc2cad0
ubaid <ubaidsaleem825@gmail.com>
2025-05-22 08:51:41 +0500

    initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('iris_model.pkl', 'rb'))
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    output = '' # Default value for GET requests
+
+    if request.method == 'POST':
+        data = [float(x) for x in request.form.values()]
+        prediction = model.predict([data])  # model expects a 2D array
+        output = "Flower will be {}".format(prediction[0])
+
+    return render_template('index.html', rsult=output)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
